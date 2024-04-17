8a4a2abac632b2625b5aedc92982d6b92652c5c1
mxnc <thibaultmaxence44@gmail.com>
2024-04-17 20:56:16 +0200

    task 8. Ambient Namespaces

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,7 @@
+import { RowElement, RowID } from './interface';
+
+export declare function insertRow(row: RowElement): number;
+
+export declare function deleteRow(rowId: RowID): void;
+
+export declare function updateRow(rowId: RowID, row: RowElement): number;
