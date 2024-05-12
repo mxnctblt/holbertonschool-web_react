928597bfbdb15036bdff3bf768f36e75bb8276e0
mxnc <thibaultmaxence44@gmail.com>
2024-05-12 18:12:45 +0200

    adding aphrodite for footer

--- React_inline_styling/task_2/dashboard/src/Footer/Footer.js
+++ React_inline_styling/task_2/dashboard/src/Footer/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { getFooterCopy, getFullYear } from '../utils/utils';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  footer: {
+    borderTop: '5px rgb(201, 45, 45) solid',
+    textAlign: 'center',
+    fontSize: '1.5rem',
+  },
+});
 
 const Footer = () => {
   const currentYear = getFullYear();
   const footerCopy = getFooterCopy(false);
   return (
-    <footer className="App-footer">
+    <footer className={css(styles.footer)}>
       <p>
         Copyright {currentYear} - {footerCopy}
       </p>
--- React_inline_styling/task_2/dashboard/src/Footer/Footer.test.js
+++ React_inline_styling/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Footer from './Footer';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+
+StyleSheetTestUtils.suppressStyleInjection();
 
 describe('tests for Footer component', () => {
   it('should render without crashing', () => {
